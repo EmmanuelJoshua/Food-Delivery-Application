09fdc581a8522c06f786b72ef21dd9d7afd596ce
Emmanuel Joshua <emmanueljoshua145@gmail.com>
2021-09-11 16:22:57 +0100

    Button Widgets and Splash Screen finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:cloudfift_assessment/utils/colors.dart';
+import 'package:cloudfift_assessment/utils/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'utils/theme.dart';
 import 'utils/navigator.dart';
 import 'pages/onboarding/onboarding.dart';
@@ -32,7 +35,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-    load();
+    // load();
     super.initState();
   }
 
@@ -43,7 +46,101 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: Finish Splashscreen
-    return Container();
+    SizeConfig().init(context);
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          Container(
+            width: SizeConfig.screenWidth,
+            height: SizeConfig.screenHeight,
+            decoration: BoxDecoration(
+              gradient: splashGradientColor,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Expanded(
+                child: Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        child: Image.asset('assets/images/splash_logo.png'),
+                        height: getProportionateResponsiveSize(109),
+                        width: getProportionateResponsiveSize(166),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 50,
+                      child: Opacity(
+                        opacity: 0.2,
+                        child: Container(
+                          child: Image.asset('assets/images/splash_bg1.png'),
+                          height: getProportionateResponsiveSize(52),
+                          width: getProportionateResponsiveSize(47),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 50,
+                      child: Opacity(
+                        opacity: 0.2,
+                        child: Container(
+                          child: Image.asset('assets/images/splash_bg1.png'),
+                          height: getProportionateResponsiveSize(52),
+                          width: getProportionateResponsiveSize(47),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 50,
+                      child: Opacity(
+                        opacity: 0.1,
+                        child: Container(
+                          child: Image.asset('assets/images/splash_bg1.png'),
+                          height: getProportionateResponsiveSize(52),
+                          width: getProportionateResponsiveSize(47),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 50,
+                      child: Container(
+                        child: Image.asset('assets/images/splash_bg2.png'),
+                        height: getProportionateResponsiveSize(52),
+                        width: getProportionateResponsiveSize(52),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(right: 30, left: 30, bottom: 10),
+                child: Wrap(
+                  children: [
+                    Text(
+                        'Nam eget molestie tincidunt massa morbi dolor quam tortor mi. Proin urna vitae facilisis tincidunt. ',
+                        softWrap: true,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.lato(
+                            color: Colors.white,
+                            fontSize: 14,
+                            height: 1.4,
+                            fontWeight: FontWeight.w400)),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
